aac1fa5e523ab58b6eb83d845472ab1fe2cdbb6e
petwillian <marizopolis02@gmail.com>
2024-01-28 17:59:50 +0000

    Arquivos gerados: base de dados (gasolina.csv), gasolina.py e gasolina.png

--- gasolina.py
+++ gasolina.py
@@ -1,21 +1,31 @@
 # código de geração do gráfico
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Carregando os dados do arquivo CSV
 dados_gasolina = pd.read_csv('gasolina.csv')
 
-with sns.axes_style('darkgrid'):
-  grafico_gasolina = sns.lineplot(
-      x = 'dia',
-      y = 'venda',
-      data = dados_gasolina
-  )
+# Configurando o estilo do gráfico com o Seaborn
+sns.set_theme(style="ticks", palette="pastel")
 
-  grafico_gasolina.set(
-      title = 'Preço médio de venda de gasolina na cidade de São Paulo',
-      xlabel = 'Dia',
-      ylabel = 'Preço (R$)'
-  )
+# Criando o gráfico de linha
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='dia', y='venda', data=dados_gasolina, label='Preço da Gasolina', color = 'green', marker = 'o' )
 
-  figura = grafico_gasolina.get_figure()
-  figura.savefig('gasolina.png', dpi=600)# código de geração do gráfico
+# Adicionando rótulos e título
+plt.title('Variação do Preço da Gasolina ao Longo do Tempo')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
+
+# Adicionando legenda
+plt.legend()
+
+# Adicionando grade
+plt.grid(True)
+
+# Salvando o gráfico como uma imagem PNG
+plt.savefig('gasolina.png')
+
+# Exibindo o gráfico
+plt.show()
